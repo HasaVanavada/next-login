97402685b001ac93a6bd9e59200c0850b19057a1
HasaVanavada <hasa.vanavada14@gmail.com>
2024-11-21 11:47:36 +0530

    orcid

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import LinkedInProvider from 'next-auth/providers/linkedin';
-import ORCIDProvider from 'next-auth/providers/orcid';
+import ORCIDProvider from 'next-auth/providers/oauth';
 
 export const authOptions = {
   providers: [
